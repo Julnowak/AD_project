37472afa0e2bbf8cc641c83cd04cc3e94325659c
Julnowak <julnowak@student.agh.edu.pl>
2024-06-26 03:03:15 +0200

    bp

--- backend/main_app/consumers.py
+++ backend/main_app/consumers.py
@@ -47,33 +47,120 @@ class Consumer(AsyncWebsocketConsumer):
         text_data_json = json.loads(text_data)
         print(text_data_json)
 
+        ##### LIMIT #####
         try:
             limit = text_data_json['limit']
         except:
             limit = 100
+
+        ##### TEMPERATURE #####
         stmt = text(f'SELECT temperature_measurements.temperature, measurements.timestamp FROM temperature_measurements JOIN measurements ON measurements.id = temperature_measurements.measurement_id JOIN sensors ON sensors.id = measurements.sensor_id limit {limit}')
         results = self.session.execute(stmt).fetchall()
-        # l = []
-        # for i in results:
-        #     l.append(l[0])
+        # print(results)
+
+        temp_y = []
+        date_x = []
+        for i in results:
+            temp_y.append(i[0])
+            date_x.append(str(i[1]))
+
+        stmt = text(
+            f'''
+                SELECT 
+                    MAX(subquery.temperature), 
+                    MIN(subquery.temperature), 
+                    AVG(subquery.temperature) 
+                FROM (
+                    SELECT temperature_measurements.temperature
+                    FROM temperature_measurements 
+                    JOIN measurements ON measurements.id = temperature_measurements.measurement_id 
+                    JOIN sensors ON sensors.id = measurements.sensor_id 
+                    LIMIT {limit}
+                ) AS subquery
+            ''')
+        minMaxAvgTemp = self.session.execute(stmt).fetchone()
+
+        # min max avg
+        print(minMaxAvgTemp)
+
+        ##### HUMIDITY #####
+        stmt = text(
+            f'SELECT temperature_measurements.humidity, measurements.timestamp FROM temperature_measurements JOIN measurements ON measurements.id = temperature_measurements.measurement_id JOIN sensors ON sensors.id = measurements.sensor_id limit {limit}')
+        results = self.session.execute(stmt).fetchall()
         print(results)
 
-        # productId = text_data_json['productId']
-        # userId = text_data_json['userId']
-        #
-        # likes = await self.set_like(productId, userId)
-        #
+        hum_y = []
+        date_humx = []
+        for i in results:
+            hum_y.append(i[0])
+            date_humx.append(str(i[1]))
+
+        stmt = text(
+            f'''
+                SELECT 
+                    MAX(subquery.humidity), 
+                    MIN(subquery.humidity), 
+                    AVG(subquery.humidity) 
+                FROM (
+                    SELECT temperature_measurements.humidity
+                    FROM temperature_measurements 
+                    JOIN measurements ON measurements.id = temperature_measurements.measurement_id 
+                    JOIN sensors ON sensors.id = measurements.sensor_id 
+                    LIMIT {limit}
+                ) AS subquery
+            ''')
+        minMaxAvgHum = self.session.execute(stmt).fetchone()
+
+        # min max avg
+        print(minMaxAvgHum)
+
+        ##### SENSORS #####
+        stmt2 = text(
+            f'SELECT * FROM sensors')
+        sensors = self.session.execute(stmt2).all()
+
+        sens = []
+        for s in sensors:
+            sens.append({"id": s[0], "name": s[1]})
+
+
+        ###### CLOUDY #####
+
+        stmt = text('''
+            SELECT 
+                CASE
+                    WHEN cloud_cover_total <= 33 THEN 'sunny'
+                    WHEN cloud_cover_total > 33 AND cloud_cover_total <= 66 THEN 'medium'
+                    ELSE 'cloudy'
+                END AS cloud_status,
+                COUNT(*) AS count
+            FROM cloud_measurements
+            GROUP BY cloud_status
+            ORDER BY count DESC 
+        ''')
+
+        cloudy_data = self.session.execute(stmt).fetchall()
+
+        print(list(s[0] for s in cloudy_data))
+
         await self.channel_layer.group_send(
-            self.socket, {'type': 'info.message', "likes": [1, 2, 3, 4, 5]}
+            self.socket, {'type': 'info.message', "temperature_plot": {"temperature": temp_y, "date": date_x,
+                                                                       "max": round(minMaxAvgTemp[0], 2), "min": round(minMaxAvgTemp[1],2), "avg": round(minMaxAvgTemp[2],2)},
+                          "humidity_plot": {"humidity": hum_y, "date": date_humx,
+                                            "max": round(minMaxAvgHum[0], 2), "min": round(minMaxAvgHum[1],2), "avg": round(minMaxAvgHum[2],2)}, "sensors": sens,
+                          "cloudy_plot": {"status": list(s[0] for s in cloudy_data), "number": list(s[1] for s in cloudy_data)}}
         )
 
     # Receive message from room group
     async def info_message(self, event):
         print(event)
-        likes = event["likes"]
+        temperature_plot = event["temperature_plot"]
+        humidity_plot = event["humidity_plot"]
+        cloudy_plot = event["cloudy_plot"]
+        sensors = event["sensors"]
 
         # Send message to WebSocket
-        await self.send(text_data=json.dumps({"likes": likes}))
+        await self.send(text_data=json.dumps({"cloudy_plot": cloudy_plot, "humidity_plot": humidity_plot, "temperature_plot": temperature_plot, "sensors": sensors}))
 
     # @sync_to_async
     # def set_like(self, productId, userId):
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,11 +3,35 @@ import './App.css';
 // @ts-ignore
 import Plot from 'react-plotly.js';
 
+// interface Sensor = {
+//     id: number,
+//     name: string
+// }
 function App() {
 
     const [socket, setSocket] = useState<WebSocket | null>(null);
     const [limit, setLimit] = useState<string | null>('100');
+
+    // temperature
     const [plotData, setPlotData] = useState([]);
+    const [maxTemp, setMaxTemp] = useState(0);
+    const [minTemp, setMinTemp] = useState(0);
+    const [avgTemp, setAvgTemp] = useState(0);
+
+    // humidity
+    const [plotHumidity, setPlotHumidity] = useState([]);
+    const [maxHum, setMaxHum] = useState(0);
+    const [minHum, setMinHum] = useState(0);
+    const [avgHum, setAvgHum] = useState(0);
+
+    // cloudy
+    const [plotCloudy, setPlotCloudy] = useState([]);
+    // const [maxHum, setMaxHum] = useState(0);
+    // const [minHum, setMinHum] = useState(0);
+    // const [avgHum, setAvgHum] = useState(0);
+
+    // sensors
+    const [sensors, setSensors] = useState([]);
 
     useEffect(() => {
 
@@ -31,12 +55,47 @@ function App() {
                 const data = JSON.parse(event.data);
                 console.log(data)
                 // @ts-ignore
+                setSensors(data.sensors)
+
+
+                // @ts-ignore
                 setPlotData([
                     // @ts-ignore
-                    { x: data.likes,
-                      y: data.likes,
+                    { x: data.temperature_plot.date,
+                      y: data.temperature_plot.temperature,
                       type: 'scatter',
+                      mode: 'markers',
+                      marker: {color: 'red'},
+                    }
+                  ]);
+
+                setMinTemp(data.temperature_plot.min)
+                setMaxTemp(data.temperature_plot.max)
+                setAvgTemp(data.temperature_plot.avg)
+
+                setPlotHumidity([
+                    // @ts-ignore
+                    { x: data.humidity_plot.date,
+                      y: data.humidity_plot.humidity,
+                      type: 'bar',
                       mode: 'lines+markers',
+                      marker: {color: 'lightblue'},
+                    }
+                  ]);
+
+                setMinHum(data.humidity_plot.min)
+                setMaxHum(data.humidity_plot.max)
+                setAvgHum(data.humidity_plot.avg)
+
+                // cloudy
+
+                // @ts-ignore
+                setPlotCloudy([
+                    // @ts-ignore
+                    { x: data.cloudy_plot.status,
+                      y: data.cloudy_plot.number,
+                      type: 'bar',
+                      mode: 'markers',
                       marker: {color: 'red'},
                     }
                   ]);
@@ -66,13 +125,102 @@ function App() {
       <h1>Hello</h1>
 
       <h3>Temperatura i wilgotność</h3>
-        <Plot
-          data={plotData}
-          layout={{ title: 'Real-Time Data' }}
-        />
+        <div style={{display: "inline-block"}}>
+            <Plot
+              data={plotData}
+              layout={{
+                title: 'Temperatura',
+                xaxis: {
+                  title: 'Data',
+                  type: 'date',
+                },
+                yaxis: {
+                  title: 'Temperatura',
+                },
+              }}
+            />
+
+            <table style={{border: "1px black solid", width: 300, margin: "auto"}} className="table">
+                <thead>
+                <tr>
+                    <th style={{border: "1px black solid"}} scope="col"></th>
+                    <th scope="col">Wartość temperatury</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <th style={{border: "1px black solid"}} scope="row">Minimalna</th>
+                    <td>{minTemp}</td>
+                </tr>
+                <tr>
+                    <th style={{border: "1px black solid"}} scope="row">Maksymalna</th>
+                    <td>{maxTemp}</td>
+                </tr>
+                <tr>
+                    <th style={{border: "1px black solid"}} scope="row">Średnia</th>
+                    <td>{avgTemp}</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+
+        <div style={{display: "inline-block"}}>
+            <Plot
+              data={plotHumidity}
+              layout={{
+                title: 'Wilgotność powietrza',
+                xaxis: {
+                  title: 'Data',
+                  type: 'date',
+                },
+                yaxis: {
+                  title: 'Wilgotność',
+                },
+              }}
+            />
+
+            <table style={{border: "1px black solid", width: 300, margin: "auto"}} className="table">
+                <thead>
+                <tr>
+                    <th style={{border: "1px black solid"}} scope="col"></th>
+                    <th scope="col">Wilgotność</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <th style={{border: "1px black solid"}} scope="row">Minimalna</th>
+                    <td>{minHum}</td>
+                </tr>
+                <tr>
+                    <th style={{border: "1px black solid"}} scope="row">Maksymalna</th>
+                    <td>{maxHum}</td>
+                </tr>
+                <tr>
+                    <th style={{border: "1px black solid"}} scope="row">Średnia</th>
+                    <td>{avgHum}</td>
+                </tr>
+                </tbody>
+            </table>
+
+        </div>
+
+
       <h3>Ciśnienie</h3>
       <h3>Wiatr</h3>
       <h3>Zachmurzenie</h3>
+        <Plot
+              data={plotCloudy}
+              layout={{
+                title: 'Zachmurzenie',
+                xaxis: {
+                  title: 'Status',
+                  type: 'category',
+                },
+                yaxis: {
+                  title: 'Zachmurzenie',
+                },
+              }}
+            />
 
       <form onSubmit={handleSubmit} style={{ width: 400, margin: "auto"}}>
         <div>
@@ -96,6 +244,7 @@ function App() {
         </div>
         <button className="btn btn-dark" type="submit" >Filtruj</button>
       </form>
+        {/*{sensors.map(d => (<li key={d.id}>{d.name}</li>))} */}
     </div>
   );
 }
